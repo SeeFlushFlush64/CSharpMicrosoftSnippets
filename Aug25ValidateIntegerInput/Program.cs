a079c264aa6200be076ff1df91f054d18e56b5b8
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2024-08-26 22:24:32 +0800

    added ValidateIntegerInputRange

---
+++ Aug25ValidateIntegerInput/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace ValidateIntegerInput
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UserPrompt();
+            GetIntegerInput();
+        }
+        static void UserPrompt()
+        {
+            Console.Write("Please enter a number between 5 and 10: ");
+        }
+        static void WarningPrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Sorry, you entered an invalid input, please try again");
+        }
+        static void DisplayValidInput(int numericValue)
+        {
+            Console.WriteLine($"{numericValue} is indeed between 5 and 10");
+        }
+        static void GetIntegerInput()
+        {
+            int numericValue = 0;
+            bool validNumber = false;
+            bool numberWithinRange = false;
+            do
+            {
+                validNumber = int.TryParse(Console.ReadLine(), out numericValue);
+                numberWithinRange = numericValue > 5 && numericValue < 10;
+                if (validNumber && numberWithinRange)
+                {
+                    break;
+                }
+                else 
+                {
+                    WarningPrompt();
+                    UserPrompt();
+                }
+            } while (!validNumber || !numberWithinRange);
+            DisplayValidInput(numericValue);
+        }
+    }
+}
