8e13b28041b9f10018d67c6b4dfefbe9cdf7c958
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2024-09-01 00:23:49 +0800

    Removing Array Elements using List Conversion

---
+++ Aug31AddingAndDeletingElementsArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace AddingAngDeletingElementsArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstNames = 
+            {
+                "Michael", "Rhey", "Owen"
+            };
+
+            //Printing of firstNames elements
+            Console.WriteLine("Original First Names");
+            foreach (string firstName in firstNames)
+            {
+                Console.WriteLine(firstName);
+            }
+
+            //Array to List conversion, using of Remove() to remove an element "Rhey"
+            List<string> listOfNames = new List<string>(firstNames);
+            listOfNames.Remove("Rhey");
+
+            //List to Array, "Michael", "Owen" are the elements only of the list will be assigned to newNames array
+            string[] newNames = listOfNames.ToArray();
+
+            //Use of spread operator, "Michael", "Owen", "Daniell", "Jake"
+            string[] updatedFirstNames = 
+            [
+                ..newNames, "Daniell", "Jake"
+
+            ];
+
+            Console.WriteLine("Updated First Names");
+            foreach (string updated in updatedFirstNames)
+            {
+                Console.WriteLine(updated);
+            }
+        }
+    }
+}
