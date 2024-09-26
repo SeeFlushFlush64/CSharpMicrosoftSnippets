49fee1f85b3b1ba58f7bbff04424b7a54a001bdc
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2024-09-26 14:49:46 +0800

    finding the index of the next occurence of an element

---
+++ September26FindingTheNextOccurenceOfAnElement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace September26FindingTheNextOccurenceOfAnElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 2, 5 };
+            int target = 2;
+            int targetIndex = 0;
+            int firstOccurence = Array.IndexOf(arr, target);
+            targetIndex = Array.IndexOf(arr, target, firstOccurence + 1);
+            System.Console.WriteLine($"The first occurence of number 2 is at index {firstOccurence}");
+            System.Console.WriteLine($"The location of number 2 is at index {targetIndex}");
+        }
+    }
+}
