b01e833b66d0b8329f9705acefc4f5c926f49150
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2024-09-26 15:26:59 +0800

    quadratic time complexity for finding 2 sums

---
+++ September26TwoSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace September26TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNum = {2, 3, 1};
+            int targetSum = 3;
+            List<int> addendsIndex = new List<int>();
+            bool addends = false;
+            for (int i = 0; i < myNum.Length; i++)
+            {
+                for (int j = i + 1; j < myNum.Length; j++)
+                {
+                    if (myNum[i] + myNum[j] == targetSum)
+                    {
+                        addendsIndex.Add(myNum[i]);
+                        addendsIndex.Add(myNum[j]);
+                    }
+                }
+            }
+            foreach(int myIndex in addendsIndex)
+            {
+                System.Console.WriteLine(myIndex);
+            }
+        }
+    }
+}
